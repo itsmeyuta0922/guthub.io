2862053b6e7cba3c4ed86c400050fb454a09de43
itsmeyuta0922 <52714813+itsmeyuta0922@users.noreply.github.com>
2019-07-11 22:05:19 +0900

    Create base.js

---
+++ base.js
@@ -0,0 +1,9 @@
+$(function() {
+  $('#vegas-slide').vegas({
+    slide:[
+      {src:'image/584438510.906002.JPG'},
+      {src:'image/IMG_4738.JPG'},
+      {src:'image/IMG_7907.JPG'}
+    ]
+  });
+});
